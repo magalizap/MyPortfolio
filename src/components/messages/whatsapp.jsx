d35d62a6d414b346ef65bcaa4b900d87e1aeb420
Magali Sol <magalisol2307@gmail.com>
2024-04-12 17:44:37 -0300

    change tel wpp

--- src/components/messages/whatsapp.jsx
+++ src/components/messages/whatsapp.jsx
@@ -3,7 +3,7 @@ import './whatsapp.css'
 import { FaWhatsapp } from "react-icons/fa";
 const whatsapp = () => {
   return (
-    <a href="https://api.whatsapp.com/send?phone=1234567891" className="float" target="_blank">
+    <a href="https://api.whatsapp.com/send?phone=5491160200142" className="float" target="_blank">
         <FaWhatsapp className='my-float'/>
     </a>
   )
